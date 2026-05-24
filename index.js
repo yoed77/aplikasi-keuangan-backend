d791b656cea048695bc3ef5f4d641ffb73a712ea
yoed77 <yoed.sr@gmail.com>
2026-05-24 20:31:26 +0700

    Export app for Vercel deployment
    Added module.exports to make the app exportable for Vercel deployment.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
+
+// MEMBUKA PINTU AKSES PENUH AGAR FRONTEND BISA MENGAMBIL DATA
 app.use(cors());
 app.use(express.json());
 
@@ -60,3 +62,6 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
+
+// BARIS SAKTI WAJIB UNTUK VERCEL (JANGAN SAMPAI HILANG):
+module.exports = app;
